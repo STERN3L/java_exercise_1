c19742eb42f2906469a5d74d6565371d62d2c339
STERN3L <danyil.bazain@epita.fr>
2022-09-20 14:07:34 +0200

    Parite 3

--- src/Launcher.java
+++ src/Launcher.java
@@ -3,15 +3,19 @@ import java.util.Scanner;
 class Launcher {
     public static void main(String[] args) {
         System.out.println("Bienvenue"); 
-        Scanner myObj = new Scanner(System.in);
-        System.out.println("Enter word :");
 
-        String word = myObj.nextLine();
 
-        if (word.equals("quit"))  {
-            return;
-        }
-        else{
+        while(true){
+
+            Scanner myObj = new Scanner(System.in);
+
+            System.out.println("Enter word :");
+            String word = myObj.nextLine();
+
+            if (word.equals("quit"))  {
+                break;
+            }
+
             System.out.println("Unknown command");
         }
     }
